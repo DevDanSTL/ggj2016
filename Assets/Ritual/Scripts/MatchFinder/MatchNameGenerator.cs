1db7462154fec8d5a20bb92ce5a35d4aecf9a038
thedeann <jordanschlechte@gmail.com>
2016-01-29 23:28:47 -0600

    Initial connection stuffs

---
+++ Assets/Ritual/Scripts/MatchFinder/MatchNameGenerator.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class MatchNameGenerator : MonoBehaviour {
+
+    public List<string> adjectives = new List<string>() {
+        "angry",
+        "nasty",
+        "slow",
+        "ginger",
+        "silly",
+        "slippery"
+    };
+
+    public List<string> nouns = new List<string>() {
+        "chicken",
+        "banana",
+        "tree",
+        "cup",
+        "matchbox"
+    };
+
+    public int min = 1;
+    public int max = 999;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	public string GenerateMatchName()
+    {
+        int adj = Random.Range(0, adjectives.Count);
+        int noun = Random.Range(0, nouns.Count);
+        int num = Random.Range(min, max);
+
+        return adjectives[adj] + nouns[noun] + num;
+    }
+}
