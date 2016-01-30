e12702ac48021b461775b377a9b14ac6e122b90a
Daniel Eichling <deichling@riotgames.com>
2016-01-30 03:34:13 -0600

    Working Boss "AI"

---
+++ Assets/Scripts/GUIUpdater.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using BehaviorDesigner.Runtime;
+
+public class GUIUpdater : MonoBehaviour
+{
+    // public static int BossHP;        // The player's score.
+
+    Text text;                      // Reference to the Text component.
+
+    void Awake ()
+    {
+        // Set up the reference.
+        text = GetComponent <Text> ();
+
+        // Reset the score.
+        // BossHP = 0;
+    }
+
+    void Update ()
+    {
+        // Set the displayed text to be the word "Score" followed by the score value.
+        text.text = "BossHP: " + GlobalVars.BossHP.Value;
+    }
+}
--- Assets/Scripts/GlobalVars.cs
+++ Assets/Scripts/GlobalVars.cs
@@ -1,29 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-
-
+using BehaviorDesigner.Runtime;
 
 public class GlobalVars : MonoBehaviour {
 
-// Team Vars
+public BehaviorTree behaviorTree;
+
+// Static Vars
+public int BurnDamage = 5;
+public float BurnTime = 2f;
+public float FreezeMod = 1.5f;
+
+// Boss Vars
+static public SharedInt BossHP;
+public int BossAtk = 20;
+public int BossHAtk = 40;
+public int BossMAtk = 30;
+public int BossSAtk = 50;
+public int BossDef = 5;
+// public var BossTarget = null; - Using Aggro for now
 public bool Rended = false;
 public bool Taunted = false;
 public bool Frozen = false;
 public bool Burning = false;
+
+// Team Vars
 public bool Empowered = false;
 public bool Invisible = false;
 
 // Mage Vars
 public int MageHP = 100;
 public int MageAtk = 5;
+public int MageAggro = 0;
 public bool MShielded = false;
 
 // Thief Vars
 public int ThiefHP = 75;
+public int ThiefAggro = 0;
 
 // Cleric Vars
 public int ClericHP = 100;
+public int ClericAggro = 0;
 
 // Warrior Vars
 public int WarriorHP = 200;
@@ -31,11 +48,12 @@ public int WarriorAtk = 20;
 public float OppMod = 1.5f;
 public bool OppAtk = false;
 public bool PShielded = false;
+public int WarriorAggro = 0;
 
 	// Use this for initialization
 	void Start () {
-
-       
+      BossHP = (SharedInt)behaviorTree.GetVariable("BossHP");
+      BossHP.Value = 500;
 	}
 	
 	// Update is called once per frame
