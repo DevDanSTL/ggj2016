1484646f25a6899491622dcbb3ac7de7e882fc6a
Daniel Eichling <deichling@riotgames.com>
2016-01-30 20:51:49 -0600

    Particles and new Hydra and stuffs

---
+++ Assets/MagicArsenal/InteractiveDemo/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LoadSceneOnClick : MonoBehaviour
+{
+    public void LoadScene1()  {
+		Application.LoadLevel ("magic_projectiles");
+	}
+    public void LoadScene2()  {
+		Application.LoadLevel ("magic_sprays");
+	}
+    public void LoadScene3()  {
+		Application.LoadLevel ("magic_aura");
+	}
+    public void LoadScene4()  {
+		Application.LoadLevel ("magic_modular");
+	}
+    public void LoadScene5()  {
+		Application.LoadLevel ("magic_domes");
+	}
+    public void LoadScene6()  {
+		Application.LoadLevel ("magic_shields");
+	}
+    public void LoadScene7()  {
+		Application.LoadLevel ("magic_sphereblast");
+	}
+    public void LoadScene8()  {
+        Application.LoadLevel("magic_enchant");
+    }
+
+}
